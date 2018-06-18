ee5a84613c9e59ccfd9713480c92ff94a20119d3
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2018-06-18 19:43:51 +0200

    get rid off strange errors

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const io = socketIo(server);
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
-});
-
 io.on("connection", function(socket) {
 	socket.on("join", function(name) {
 		userService.addUser({
